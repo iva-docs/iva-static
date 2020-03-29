9b35ad3280d9a8feb623e7a001ecd21036c2fd63
khaled romdhane <khaled@medgo.fr>
2020-03-29 20:14:19 +0200

    adding drift script

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [{ src: "/drift.js" }]
   },
   /*
    ** Customize the progress bar color
@@ -36,6 +37,5 @@ module.exports = {
         });
       }
     }
-  },
-  plugings: [{ src: "~plugins/drift.js", mode: "client" }]
+  }
 };

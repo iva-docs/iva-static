507d7a95c1f66f74069c1b8464350936d5716e0a
khaled romdhane <khaled@medgo.fr>
2020-04-14 21:16:54 +0200

    blog

---
+++ pages/blog/_id.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container content box" style="margin-top:100px">
+    <RichTextRenderer :document="content" />
+  </div>
+</template>
+
+<script>
+import Contentful from "../../plugins/contentful.js";
+import RichTextRenderer from "contentful-rich-text-vue-renderer";
+
+export default {
+  data() {
+    return {
+      title: "",
+      content: {
+        nodeType: "document",
+        content: [
+          {
+            nodeType: "paragraph",
+            content: [
+              {
+                nodeType: "text",
+                value: "Hello",
+                marks: [{ type: "bold" }]
+              },
+              {
+                nodeType: "text",
+                value: " world!",
+                marks: [{ type: "italic" }]
+              }
+            ]
+          }
+        ]
+      }
+    };
+  },
+  async mounted() {
+    const client = Contentful.createClient();
+    const resBlog = await client.getEntry(this.$route.params.id);
+    document.title = "Iva - " + resBlog.fields.title;
+    this.content = resBlog.fields.content;
+  },
+  methods: {
+    renderContent(node) {
+      if (node.value) {
+        let str = node.value;
+        return str + " " + this.renderContent(node.content);
+      }
+      return "";
+    }
+  },
+  components: {
+    RichTextRenderer
+  }
+};
+</script>

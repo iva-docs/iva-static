7e8a15e607aa0a6d399d60eed7676ba9c1e80db3
khaled romdhane <khaled@medgo.fr>
2020-05-23 01:46:12 +0200

    adding popup

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -40,6 +40,32 @@ export default {
     document.title = "Iva - " + resBlog.fields.title;
     return { content: resBlog.fields.content };
   },
+  mounted() {
+    (function(m, a, i, l, e, r) {
+      m["MailerLiteObject"] = e;
+      function f() {
+        var c = { a: arguments, q: [] };
+        var r = this.push(c);
+        return "number" != typeof r ? r : f.bind(c.q);
+      }
+      f.q = f.q || [];
+      m[e] = m[e] || f.bind(f.q);
+      m[e].q = m[e].q || f.q;
+      r = a.createElement(i);
+      var _ = a.getElementsByTagName(i)[0];
+      r.async = 1;
+      r.src = l + "?v" + ~~(new Date().getTime() / 1000000);
+      _.parentNode.insertBefore(r, _);
+    })(
+      window,
+      document,
+      "script",
+      "https://static.mailerlite.com/js/universal.js",
+      "ml"
+    );
+
+    var ml_account = ml("accounts", "2003320", "k7r4d1e0o8", "load");
+  },
   methods: {
     renderContent(node) {
       if (node.value) {
